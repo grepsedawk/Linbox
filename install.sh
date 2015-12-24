acba2291204b2695b6abcc3413de78527dcc02d0
Alex Piechowski <alex@powercode.com>
2015-12-23 21:44:01 -0600

    Create base install script

---
+++ install.sh
@@ -0,0 +1,113 @@
+#!/bin/bash -
+#title          :linbox
+#description    :It's winbox for linux!
+#author         :Alex Piechowski
+#date           :20151223
+#version        :0.1
+#usage          :./install.sh
+#notes          :
+#============================================================================
+
+BOOTUP=color
+RES_COL=71
+MOVE_TO_COL="echo -en \\033[${RES_COL}G"
+SETCOLOR_SUCCESS="echo -en \\033[0;32m"
+SETCOLOR_FAILURE="echo -en \\033[0;31m"
+SETCOLOR_WARNING="echo -en \\033[0;33m"
+SETCOLOR_NORMAL="echo -en \\033[0;39m"
+
+#####
+#
+#  Usage: action "Thing you want to say" funcName
+#
+#####
+#usage "action "Thing you want to say" funcName
+action() {
+	STRING=$1
+	echo -n "$STRING "
+	shift
+	startSpinner
+	eval "$*" 2>> /tmp/powercodeInstall.log && echo_success || echo_failure
+	stopSpinner
+	rc=$?
+	echo
+	return $rc
+}
+
+echo_success() {
+  [ "$BOOTUP" = "color" ] && $MOVE_TO_COL
+  echo -n "[  "
+  [ "$BOOTUP" = "color" ] && $SETCOLOR_SUCCESS
+  echo -n $"OK"
+  [ "$BOOTUP" = "color" ] && $SETCOLOR_NORMAL
+  echo -n "   ]"
+  echo -ne "\r"
+  return 0
+}
+
+echo_failure() {
+  [ "$BOOTUP" = "color" ] && $MOVE_TO_COL
+  echo -n "[ "
+  [ "$BOOTUP" = "color" ] && $SETCOLOR_FAILURE
+  echo -n $"ERROR"
+  [ "$BOOTUP" = "color" ] && $SETCOLOR_NORMAL
+  echo -n " ]"
+  echo -ne "\r"
+  return 1
+}
+
+function _spinner() {
+    case $1 in
+        start)
+
+            # start spinner
+            i=1
+            sp='\|/-'
+            delay=0.15
+
+            while :
+            do
+                [ "$BOOTUP" = "color" ] && $MOVE_TO_COL
+                printf "[   ${sp:i++%${#sp}:1}   ]"
+                sleep $delay
+            done
+            ;;
+        stop)
+            if [[ -z ${2} ]]; then
+                exit 0
+            fi
+            kill $2 > /dev/null 2>&1
+            ;;
+        *)
+            echo "invalid argument, try {start/stop}"
+            exit 1
+            ;;
+    esac
+}
+
+function startSpinner {
+    _spinner "start" "${1}" &
+    _sp_pid=$!
+    disown
+}
+
+function stopSpinner {
+    _spinner "stop" $_sp_pid
+    unset _sp_pid
+}
+
+#####
+#
+#  Functions
+#
+#####
+function functionName() {
+    touch test
+}
+
+#####
+#
+#  Actions
+#
+#####
+action "Running function functionName" functionName
