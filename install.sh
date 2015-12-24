270a1a86cca6ea59a789d3e9911f8929fda80478
Alex Piechowski <alex@powercode.com>
2015-12-24 08:53:58 -0600

    Host winbox.exe in the repo instead of pulling it from Mikrotik's website

--- install.sh
+++ install.sh
@@ -139,7 +139,7 @@ function installWine() {
 
 function downloadWinbox() {
     mkdir /opt/winbox &>> linboxInstall.log
-    wget -O /opt/winbox/winbox.exe http://download2.mikrotik.com/routeros/winbox/3.0/winbox.exe &>> linboxInstall.log
+    cp -f bin/winbox.exe /opt/winbox/winbox.exe &>> linboxInstall.log
 }
 
 function installLinbox() {
